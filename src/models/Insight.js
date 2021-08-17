a8d1cf19dfbc325068e0c3dfab4462b4de5a931b
Felipe Menezes <felipemenezes.web@gmail.com>
2021-08-17 03:07:55 -0300

    Adiciomando campo de tags para os insights.

--- src/models/Insight.js
+++ src/models/Insight.js
@@ -1,10 +1,12 @@
 const mongoose = require('../db');
+const Tag = require('./Tag');
 const InsightSchema = new mongoose.Schema({
   texto: {
     type: String,
     required: true,
     trim: true
-  }
+  },
+  tags: [Tag.schema]
 }, {
   timestamps: {
     createdAt: 'data_criacao',
---
+++ src/models/Tag.js
@@ -0,0 +1,14 @@
+const mongoose = require('../db');
+const TagSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        uniqueCaseInsensitive: true
+    }
+});
+
+const Tag = mongoose.model('Tag', TagSchema);
+
+module.exports = Tag;
